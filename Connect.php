881151bbf7cb5767e846698cf743dacda71a66b0
Jiyifeng <306169085@qq.com>
2017-04-26 15:53:59 +1000

    change mysql to mysqli

--- Connect.php
+++ Connect.php
@@ -5,9 +5,8 @@
     $db_password="keyocijonaca";//DB password
     $db_name = "travel";//DB name
 
-    $connect = mysql_connect($server,$db_username,$db_password,$db_name);//Connect DB
+    $connect = new mysqli($server,$db_username,$db_password,$db_name);//Connect DB
     if(!$connect){
         die("can't connect".mysql_error());// Connect fail
     }
-    mysql_select_db('travel',$connect);// Name of the DB set at "travel"
 ?>
