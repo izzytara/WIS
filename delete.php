b8c45b513a8ca11e84cbf7c1e74c627368854b8d
Jiyifeng <306169085@qq.com>
2017-05-12 21:11:36 +1000

    bug

--- delete.php
+++ delete.php
@@ -3,7 +3,7 @@
     if(isset($_POST["yes"])){
     	include('Connect.php');//Connect DB
     	$storyid = $_POST["p_id"];
-    	$deletesql = "DELETE FROM travel_story WHERE stroyid = '$storyid'";
+    	$deletesql = "DELETE FROM travel_story WHERE storyid = '$storyid'";
     	$deleteres = mysqli_query($connect, $deletesql);
     	if($deleteres){
     		echo "<script>alert('Delete success！');history.go(-1);</script>"; 
