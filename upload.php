3064229d0e023cc7755c0f6019609066fc859022
Jiyifeng <306169085@qq.com>
2017-05-12 16:08:39 +1000

    添加注释

--- upload.php
+++ upload.php
@@ -1,5 +1,6 @@
 <?php
 header("Content-Type: text/html; charset=utf8");
+//Check file type is correcy
 if ((($_FILES["file"]["type"] == "image/gif")
 || ($_FILES["file"]["type"] == "image/jpeg")
 || ($_FILES["file"]["type"] == "image/jpg")
@@ -24,25 +25,25 @@ if ((($_FILES["file"]["type"] == "image/gif")
       {
       move_uploaded_file($_FILES["file"]["tmp_name"], "storyimg/" . $_FILES["file"]["name"]);
       session_start();
-      include('Connect.php');
-      if($_SESSION['auth']){
+      include('Connect.php');//Connect DB
+      if($_SESSION['auth']){//Get uid if logged in
         $uname = $_SESSION['Username'];
         $findidbyname = "SELECT uid FROM travel_user WHERE uname= '$uname'";
         $uidres = mysqli_query($connect, $findidbyname);
         $uid1 = mysqli_fetch_assoc($uidres);
         $uid = $uid1[uid];
-      }else{
+      }else{//If not logged in
         $uid = null;
       }        
-      $update_time = date("Y-m-d");
+      $update_time = date("Y-m-d");//Get current time
       $location = $_POST["location"];
       $title = $_POST["title"];
       $story = $_POST["story"];
-      $image_URL = "storyimg/". $_FILES["file"]["name"];
+      $image_URL = "storyimg/". $_FILES["file"]["name"];//Get img stored URL
       $pupular = 0;
       $sql = "INSERT INTO travel_story(storyid,uid,update_time,location,title,story,image_URL,popular) VALUES (NULL,'$uid','$update_time','$location','$title','$story','$image_URL','$pupular')";
       $res = mysqli_query($connect, $sql);
-      if($res){
+      if($res){//If save success
         echo "<script>alert('Success Save this story');</script>";
         $index = "https://infs3202-d1sr7.uqcloud.net/";  
         echo "<script type='text/javascript'>";  
@@ -54,7 +55,7 @@ if ((($_FILES["file"]["type"] == "image/gif")
       }
     }
   }
-else
+else//If get file error
   {
   echo "Invalid file";
   }
