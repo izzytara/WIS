2fce8275633e7c133285847c9f24016afd5d7bcb
Jing Zhang <j.zhang8@uq.net.au>
2017-05-12 14:39:27 +1000

    delete popular works div in index.php

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     <!-- Link Bootstrap's js -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <!-- Link Siper's js-->
-    <script src="JS/swiper.js"></script>
+    <script src="js/swiper.js"></script>
 	</head>
 
 	<!--Here is the body-->
@@ -63,7 +63,7 @@
         </div>
         
     <!--Here is the login form-->
-    <div id="userlogin" class="modal">
+        <div id="userlogin" class="modal">
             <div class="login">
                 <form class="modal-content animate" action="login.php" method="post">
                     <h2 class="tcenter">Login</h2>
@@ -85,7 +85,7 @@
         </div>
 
     <!--Here is the content-->
-    <div class="content">
+        <div class="content">
     <!--Here is the first swipper to display sample of diary -->
         <div  class="container">
     <!-- Swiper -->
@@ -145,58 +145,6 @@
             </div>
         </div>-->
 
-    <!--Here is test popular works display container-->
-        <?php
-        header("Content-Type: text/html; charset=utf8");
-        include('Connect.php');
-        $popular = "SELECT * FROM travel_story order by `popular` DESC LIMIT 10;";
-        $popularres = mysqli_query($connect, $popular);
-        while($popular1 = mysqli_fetch_assoc($popularres)){
-        ?>
-        <!--Get All stroies, sorted by popular-->
-        <div id="popular-works" class="container white">
-            <div class="diary-display">
-                <div class="photo">
-                    <img class="img-responsive" src="<?php echo $popular1[image_URL];?>" id="<?php echo $popular1[storyid];?>" alt="placeholder">
-                </div>
-                <div class="diary">
-                    <div class="location">
-                        <p>
-                            <span class="glyphicon glyphicon-map-marker"></span>
-                            <span><?php echo $popular1[location];?></span>
-                        </p>
-                    </div>                    
-                    <div class="title">
-                        <p>Title: <span><?php echo $popular1[title];?></span></p>
-                    </div>
-                    <!--Get author name from uid-->
-                    <?php
-                    $findunamebyuid = "SELECT uname FROM travel_user WHERE uid= '$popular1[uid]'";
-                    $nameres = mysqli_query($connect, $findunamebyuid);
-                    $nameres1 = mysqli_fetch_assoc($nameres);
-                    ?>
-                    <div class="author-date">
-                        <p>By:&nbsp;<span><?php echo $nameres1[uname];?></span>&nbsp;Date: <span><?php echo $popular1[update_time];?></span></p>
-                    </div>
-                    
-                    <div class="text">
-                        <p><?php echo $popular1[story];?></p>
-                    </div>                
-                </div>                           
-            </div>
-
-            <div class="vote-like">
-                    <p>
-                        <a href="#"><span class="glyphicon glyphicon-heart"></span></a>
-                        <span><?php echo $popular1[popular];?></span>Like
-                        <a href="#"><span class="glyphicon glyphicon-share"></span></a>   
-                    </p>  
-            </div>
-        </div>
-        <?php
-        }
-        ?>
-
     <!--Here is the first display container-->
         <div id="step1" class="container green">
             <p>
@@ -232,15 +180,15 @@
     </div>
     <!--Here is the footer-->
     
-    <div class="footer">
+        <div class="footer">
         <div class="container">
                 &copy; Copyright 2017
         </div>
     </div>
 
     <!-- Initialize Swiper -->
-    <script>
-        var swiper = new Swiper('.swiper-container', {
+        <script>
+            var swiper = new Swiper('.swiper-container', {
             pagination: '.swiper-pagination',
             nextButton: '.swiper-button-next',
             prevButton: '.swiper-button-prev',
@@ -249,18 +197,18 @@
             spaceBetween: 30,
             loop: true
         });
-    </script>
+        </script>
         
-    <script>
+        <script>
     //get the modal
-        var modal = document.getElementById('userlogin');
+            var modal = document.getElementById('userlogin');
     //when the user clicks anywhere out side the modal, close it
-        window.onclick = function(event){
-            if (event.target == modal){
-            modal.style.display = "none";
+            window.onclick = function(event){
+                if (event.target == modal){
+                modal.style.display = "none";
+            }
         }
-    }
-    </script>
+        </script>
 
 	</body>
 </html>
