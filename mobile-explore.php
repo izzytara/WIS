d96df531cc8aca058780ba2eb26e33792ce009c5
Jiyifeng <306169085@qq.com>
2017-05-28 20:52:49 +1000

    PHP输出json

---
+++ mobile-explore.php
@@ -0,0 +1,11 @@
+<?php
+    header("Content-Type: text/html; charset=utf8");
+    include('Connect.php');
+    $popular = "SELECT * FROM travel_story order by `popular` DESC;";
+    $arr = array();
+    $popularres = mysqli_query($connect, $popular);
+    while($popular1 = mysqli_fetch_assoc($popularres)){
+    	array_push($arr,$popular1);
+    }
+    echo json_encode($arr); 
+?>
